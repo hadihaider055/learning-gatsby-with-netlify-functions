787d4ae0e119e5c8948fb6a7a3b523cd4ab4c0e3
hadihaider055 <haiderhadi055@gmail.com>
2021-10-30 09:05:41 +0500

    using library with netlify functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const IndexPage = () => {
   const [data, setData] = useState("");
+  const [name, setName] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -9,12 +10,19 @@ const IndexPage = () => {
       const body = await response.json();
       setData(body.message);
     })();
+
+    (async () => {
+      const response = await fetch(".netlify/functions/useLibrary");
+      const body = await response.json();
+      setName(body.message);
+    })();
   }, []);
 
   return (
     <>
       <div>Hello</div>
       <div>{data}</div>
+      <div>{name}</div>
     </>
   );
 };
