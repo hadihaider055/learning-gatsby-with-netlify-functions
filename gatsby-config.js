d1020c1852865a9eb7d4a0a8ce355c86682a69a2
hadihaider055 <haiderhadi055@gmail.com>
2021-10-30 08:34:23 +0500

    "Added typescript Plugin:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby_serverless_functions",
-  },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+  ],
 };
